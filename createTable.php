620ea7ae3a72ad4f4d32a1b5f24f55ed5f92a3d1
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-12-05 21:03:50 +0700

    Make it better

---
+++ createTable.php
@@ -0,0 +1,18 @@
+<?php
+//controllers/frontend/login/createUserTable.php
+
+function createTable($f3){
+    $sql = "CREATE TABLE IF NOT EXISTS posts (
+        id TEXT,
+        title TEXT,
+        content TEXT,
+        category TEXT,
+        thumb TEXT,
+        postDate TEXT,
+        video TEXT
+    )";
+
+    $f3->get('DB')->exec($sql);
+
+    //$user = new DB\SQL\Mapper($f3->get('DB'),'posts');
+}
